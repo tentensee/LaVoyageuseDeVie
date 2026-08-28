ff1d1ac1371d0742a8d800be5a0a111a41a80ebe
LaVoyageusedeVie <lavoyageusedevie@gmail.com>
2026-08-28 14:51:12 +0200

    Enhance About section with support details
    Added details about personalized support and care for the elderly.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,13 @@ export default function About() {
         <div>
           <h3 className="about-quote">" Être là, simplement "</h3>
           <p className="about-text">Après de nombreuses années consacrées à l'accompagnement humain dans l'Éducation nationale en tant que Professeure puis Conseillère principale d'éducation, je mets aujourd’hui mes compétences d'écoute, de relation humaine et d’accompagnement au service des personnes âgées et des aidants en apportant une présence chaleureuse, bienveillante et rassurante aux personnes âgées tout en offrant une bulle de répit essentielle à leurs proches aidants.</p>
-          <p className="about-text">Pour les personnes qui souhaitent rompre l'isolement, maintenir un lien social ou simplement partager un moment de qualité, je vous propose un accompagnement humain et personnalisé dans votre quotidien avec une présence et une écoute véritable :</p>
+          <p className="about-text">Mon accompagnement
+•	Un accompagnement individualisé 
+•	Une présence authentique et rassurante 
+•	Une écoute sans jugement
+•	Respect de la personne et de son histoire
+•	Discrétion et confidentialité
+</p>
           <p className="about-text">Chaque parcours de vie mérite une attention singulière. C'est pourquoi je m'adapte précisément à vos habitudes, à votre rythme et à vos besoins spécifiques au quotidien.</p>
         </div>
       </div>

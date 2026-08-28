39e7c9bdc92dab22857cbc4e61486a91954c36bd
LaVoyageusedeVie <lavoyageusedevie@gmail.com>
2026-08-28 14:36:51 +0200

    Update manifesto text for clarity and depth

--- src/components/Manifesto.jsx
+++ src/components/Manifesto.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Manifesto() {
   return (
     <div className="manifesto-banner">
-      <p className="manifesto-text handwritten-font">Être là, simplement.</p>
+      <p className="manifesto-text handwritten-font">Parce que certains moments de la vie demandent simplement une présence attentive et humaine</p>
     </div>
   );
 }

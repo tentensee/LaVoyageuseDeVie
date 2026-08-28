1854f3da54617cbea1983b50294fff85b7e01e5b
LaVoyageusedeVie <lavoyageusedevie@gmail.com>
2026-08-28 14:57:45 +0200

    Refine description in About component
    Removed redundancy in the description of services provided to the elderly.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         </div>
         <div>
           <h3 className="about-quote">" Être là, simplement "</h3>
-          <p className="about-text">Après de nombreuses années consacrées à l'accompagnement humain dans l'Éducation nationale en tant que Professeure puis Conseillère principale d'éducation, je mets aujourd’hui mes compétences d'écoute, de relation humaine et d’accompagnement au service des personnes âgées et des aidants en apportant une présence chaleureuse, bienveillante et rassurante aux personnes âgées tout en offrant une bulle de répit essentielle à leurs proches aidants.</p>
+          <p className="about-text">Après de nombreuses années consacrées à l'accompagnement humain dans l'Éducation nationale en tant que Professeure puis Conseillère principale d'éducation, je mets aujourd’hui mes compétences d'écoute, de relation humaine et d’accompagnement au service des personnes âgées en apportant une présence chaleureuse, bienveillante et rassurante aux personnes âgées tout en offrant une bulle de répit essentielle à leurs proches aidants.</p>
           <p className="about-text">•	Un accompagnement individualisé </p>
           <p className="about-text">•	Une présence authentique et rassurante </p>
           <p className="about-text">•	Une écoute sans jugement </p>
